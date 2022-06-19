e8a4563b82b1b934823b54cd8f76bba85763385d
Roy Ben Aviv <roybenavivv@gmail.com>
2022-06-19 14:03:32 +0300

    added styles and router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,18 @@
-import React, { useEffect, useRef, useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
+import React from 'react'
+import { HashRouter, HashRouter as Router, Route, Switch } from 'react-router-dom'
+import { HomePage } from './pages/HomePage.jsx'
 const App = () => {
-  const [counter, setCounter] = useState(1);
-
-  useEffect(() => {
-    console.log(counter);
-  }, [counter]);
-
-  const buttonRef = useRef();
-
-  const setCount = () => {
-    setCounter(counter + 1);
-    console.log(counter);
-    buttonRef.current.style.width = counter * 40 + "px";
-    buttonRef.current.style.height = counter * 30 + "px";
-    console.log(buttonRef?.current.style.width);
-  };
-
   return (
     <div className="App">
-      <header className="App-header">
-        {counter}
-        <button
-          onClick={() => setCount()}
-          style={{ width: "50px" }}
-          ref={buttonRef}
-        >
-          Add
-        </button>
-      </header>
+      <HashRouter>
+        <Router>
+          <Switch>
+            <Route path="/" component={HomePage} />
+          </Switch>
+        </Router>
+      </HashRouter>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const HomePage = () => {
+
+    return (
+        <main>
+            <h1>hello furman</h1>
+        </main>
+    )
+}
