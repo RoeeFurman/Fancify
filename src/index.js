beb06e0624d711a7c765431d84dbd550476f0cb7
Roee Furman <roeefurman1@gmail.com>
2022-07-03 17:08:40 +0300

    fix bug

--- src/index.js
+++ src/index.js
@@ -4,11 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-Amplify.configure(awsconfig)
 root.render(
         <App />
 );
--- src/services/youtubeService.js
+++ src/services/youtubeService.js
@@ -5,8 +5,6 @@ export const youtubeService = {
 }
 
 async function query(value) {
-    console.log(value, 'service')
-    console.log(process.env, 'kk')
     const res = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${process.env.REACT_APP_API_KEY}&q=${value}`)
     return res.data
 }
