b79bc50bcea1e385e455206fdb39d99577e877ee
Roee Furman <roeefurman1@gmail.com>
2022-07-08 16:44:57 +0300

    fix player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -135,50 +135,64 @@ class _Player extends React.Component {
                   height="1px"
                   width="1px"
                 />
-                <button onClick={() => this.toggleLooping()}>
-                  {isLooping ? (
-                    <i className="fa-solid fa-xmark"></i>
-                  ) : (
-                    <svg role="img" height="16" width="16" viewBox="0 0 16 16">
-                      <path d="M0 4.75A3.75 3.75 0 013.75 1h8.5A3.75 3.75 0 0116 4.75v5a3.75 3.75 0 01-3.75 3.75H9.81l1.018 1.018a.75.75 0 11-1.06 1.06L6.939 12.75l2.829-2.828a.75.75 0 111.06 1.06L9.811 12h2.439a2.25 2.25 0 002.25-2.25v-5a2.25 2.25 0 00-2.25-2.25h-8.5A2.25 2.25 0 001.5 4.75v5A2.25 2.25 0 003.75 12H5v1.5H3.75A3.75 3.75 0 010 9.75v-5z"></path>
-                    </svg>
-                  )}
-                </button>
-                <FaBackward onClick={() => this.onBackward()} />
-                <button className="play" onClick={() => this.setPlaying()}>
-                  {isPlaying ? (
-                    <svg
-                      role="img"
-                      height="16"
-                      width="16"
-                      className="pause-svg"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M2.7 1a.7.7 0 00-.7.7v12.6a.7.7 0 00.7.7h2.6a.7.7 0 00.7-.7V1.7a.7.7 0 00-.7-.7H2.7zm8 0a.7.7 0 00-.7.7v12.6a.7.7 0 00.7.7h2.6a.7.7 0 00.7-.7V1.7a.7.7 0 00-.7-.7h-2.6z"></path>
-                    </svg>
-                  ) : (
-                    <svg
-                      role="img"
-                      height="16"
-                      width="16"
-                      className="play-svg"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M3 1.713a.7.7 0 011.05-.607l10.89 6.288a.7.7 0 010 1.212L4.05 14.894A.7.7 0 013 14.288V1.713z"></path>
-                    </svg>
-                  )}
-                </button>
-                <FaForward onClick={() => this.onForward()} />
-                <button onClick={() => this.setMute()}>
-                  {isMuted ? (
-                    <i className="fa-solid fa-volume-high"></i>
-                  ) : (
-                    <i className="fa-solid fa-volume-xmark"></i>
-                  )}
-                </button>
-                <button onClick={() => this.setStop()}>
-                  <i className="fa-solid fa-stop"></i>
-                </button>
+                <div className="left-btns">
+                  <button
+                    onClick={() => this.toggleLooping()}
+                    className="main-btn"
+                  >
+                    {isLooping ? (
+                      <i className="fa-solid fa-xmark"></i>
+                    ) : (
+                      <svg
+                        role="img"
+                        height="16"
+                        width="16"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M0 4.75A3.75 3.75 0 013.75 1h8.5A3.75 3.75 0 0116 4.75v5a3.75 3.75 0 01-3.75 3.75H9.81l1.018 1.018a.75.75 0 11-1.06 1.06L6.939 12.75l2.829-2.828a.75.75 0 111.06 1.06L9.811 12h2.439a2.25 2.25 0 002.25-2.25v-5a2.25 2.25 0 00-2.25-2.25h-8.5A2.25 2.25 0 001.5 4.75v5A2.25 2.25 0 003.75 12H5v1.5H3.75A3.75 3.75 0 010 9.75v-5z"></path>
+                      </svg>
+                    )}
+                  </button>
+                  <FaBackward
+                    onClick={() => this.onBackward()}
+                    className="main-btn"
+                  />
+                </div>
+                <div className="play-btn">
+                  <button className="play" onClick={() => this.setPlaying()}>
+                    {isPlaying ? (
+                      <svg
+                        role="img"
+                        height="16"
+                        width="16"
+                        className="pause-svg"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M2.7 1a.7.7 0 00-.7.7v12.6a.7.7 0 00.7.7h2.6a.7.7 0 00.7-.7V1.7a.7.7 0 00-.7-.7H2.7zm8 0a.7.7 0 00-.7.7v12.6a.7.7 0 00.7.7h2.6a.7.7 0 00.7-.7V1.7a.7.7 0 00-.7-.7h-2.6z"></path>
+                      </svg>
+                    ) : (
+                      <svg
+                        role="img"
+                        height="16"
+                        width="16"
+                        className="play-svg"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M3 1.713a.7.7 0 011.05-.607l10.89 6.288a.7.7 0 010 1.212L4.05 14.894A.7.7 0 013 14.288V1.713z"></path>
+                      </svg>
+                    )}
+                  </button>
+                </div>
+                <div className="right-btns">
+                  <FaForward
+                    onClick={() => this.onForward()}
+                    className="main-btn"
+                  />
+
+                  <button onClick={() => this.setStop()} className="main-btn">
+                    <i className="fa-solid fa-stop"></i>
+                  </button>
+                </div>
               </div>
               <div className="duration">
                 <h4>
@@ -200,6 +214,13 @@ class _Player extends React.Component {
               </div>
             </div>
             <div className="right-player">
+              <button onClick={() => this.setMute()}>
+                {isMuted ? (
+                  <i className="fa-solid fa-volume-high"></i>
+                ) : (
+                  <i className="fa-solid fa-volume-xmark"></i>
+                )}
+              </button>
               <input
                 type="range"
                 min={0}
