66b06ea134e3ea90eeb93b3b5912bad9e6f45ea6
Roee Furman <roeefurman1@gmail.com>
2022-07-17 17:04:24 +0300

    FEAT: like / play playlists

--- src/components/PlaylistPreview.jsx
+++ src/components/PlaylistPreview.jsx
@@ -1,23 +1,33 @@
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { setMiniPlaylist } from "../store/actions/audio-player.action";
+import { setMiniPlaylist, setSong } from "../store/actions/audio-player.action";
 
 export const PlaylistPreview = ({ playlist }) => {
   const history = useHistory();
   const dispatch = useDispatch();
 
-  const openPlaylist = (playlist) => {
+  const openPlaylist = (ev, playlist, isPlaying = false) => {
+    ev.stopPropagation();
     history.push(`/screen/library/${playlist.id}/`);
-    dispatch(setMiniPlaylist(playlist.id, 0, playlist.songs, playlist.name));
+    if (isPlaying) {
+      dispatch(setMiniPlaylist(playlist.id, 0, playlist.songs, playlist.name));
+      dispatch(setSong(playlist.songs[0]));
+    }
   };
 
   return (
     <section className="playlist-preview">
       {playlist && (
-        <div className="playlist-card" onClick={() => openPlaylist(playlist)}>
-          <div className="card-img">
+        <div className="playlist-card">
+          <div
+            className="card-img"
+            onClick={(ev) => openPlaylist(ev, playlist)}
+          >
             <img src={playlist.imgUrl} alt={playlist.name} />
-            <div className="play-div">
+            <div
+              className="play-div"
+              onClick={(ev) => openPlaylist(ev, playlist, true)}
+            >
               <svg
                 role="img"
                 height="16"
